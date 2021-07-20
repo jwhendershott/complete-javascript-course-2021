40b8836d4369cdc1c8aea30e29b4488967193d91
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-19 23:10:31 -0500

    Completed lesson 10

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,22 @@
 'use strict'
 
-    let js = 'amazing';
-    if (js === 'amazing') alert('Javascript is fun');
+// let js = 'amazing';
 
-    40 + 8 + 23 - 10; 
-    console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('jonas');
+// console.log(23);
+
+let firstName = 'Jonas';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programemr';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+
