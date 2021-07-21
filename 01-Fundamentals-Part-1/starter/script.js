df1e5fa5b1d24dd28ecab3d0bf11f374c02ebaf7
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-20 22:48:02 -0500

    completed lesson 12

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -16,9 +16,45 @@
 // let myCurrentJob = 'Teacher';
 
 // let job1 = 'Programemr';
-// let job2 = 'Teacher';
+let job2 = 'Teacher';
 
 // console.log(myFirstJob);
+
+// true;
+// console.log(true);
+
+//sets javaScriptIsFun as boolean
+let javaScriptIsFun = true
+console.log(javaScriptIsFun);
+
+//logs typeof to console
+console.log(typeof javaScriptIsFun)
+console.log(typeof true);
+console.log(typeof job2);
+console.log(typeof 'this should say string');
+
+//changes javaScriptIsFun to string
+javaScriptIsFun = 'YES!'
+
+//logs new value and typeof for javaScriptIsFun
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun)
+
+//defines year as empty variable
+let year;
+
+//logs year value and typeof as undefined
+console.log(year);
+console.log(typeof year);
+
+//defines new value to year
+year = 1991
+
+//logs year value and typeof
+console.log(year);
+console.log(typeof year);
+
+
 
 //////////
 
