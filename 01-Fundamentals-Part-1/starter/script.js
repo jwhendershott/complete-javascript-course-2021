92c9b58f638389e9e569126489b7832468495cac
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-23 20:06:57 -0500

    completed lesson 21

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -163,31 +163,70 @@
 // console.log(century);
 
 //Type conversion
-const inputYear = '1991';
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
-console.log(typeof inputYear, NaN);
+// console.log(Number('Jonas'));
+// console.log(typeof inputYear, NaN);
 
-console.log(String(23));
+// console.log(String(23));
 
-//Type coercion
-console.log('I am ' + 23 + ' years old');
-console.log('I am ' + '23' + ' years old');
-console.log('I am ' + String(23) + ' years old');
+// //Type coercion
+// console.log('I am ' + 23 + ' years old');
+// console.log('I am ' + '23' + ' years old');
+// console.log('I am ' + String(23) + ' years old');
 
-console.log('23' - '10' - 3);
-console.log('23' * '2');
-console.log('23' > '18');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2');
+// console.log('23' > '18');
 
-let n = '1' + 1;
-console.log(typeof n);
-n = n - 1; /*'11' - 1 = 10 */
-console.log(n);
+// let n = '1' + 1;
+// console.log(typeof n);
+// n = n - 1; /*'11' - 1 = 10 */
+// console.log(n);
 
-console.log(2+3+4+'5') /*(2+3+4)+'5' = 95 */
+// console.log(2+3+4+'5') /*(2+3+4)+'5' = 95 */
 
+//5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// let money = 0;
+
+// if(money){
+//     console.log(`Don't spend it all.`);
+// } else {
+//     console.log('You shoudld get a job.');
+// };
+
+// money = 5000
+
+// if(money){
+//     console.log(`Don't spend it all.`);
+// } else {
+//     console.log('You shoudld get a job.');
+// };
+
+// let height;
+
+// if (height) {
+//     console.log(`Height is defined`);
+// } else {
+//     console.log(`Height is UNDEFINED`);
+// };
+
+// height = '5 feet'
+
+// if (height) {
+//     console.log(`Height is defined`);
+// } else {
+//     console.log(`Height is UNDEFINED`);
+// };
 
 //////////
 
