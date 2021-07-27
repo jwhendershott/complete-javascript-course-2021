1e29f3d59f4f7869d1c7ff457bcab75789fa7445
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-26 23:55:56 -0500

    completed lesson 26

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -285,7 +285,7 @@
 //     console.log(`Someone else should drive.`)
 // }
 
-const day = 'monday';
+const day = 'sunday';
 
 switch(day) {
     case 'monday':
@@ -310,7 +310,20 @@ switch(day) {
         console.log('Not a valid day');
 }; 
 
-
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if ((day === 'wednesday') || (day === 'thursday')) {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if  ((day === 'saturday') || (day === 'sunday'))  { 
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day')
+}
 
 //////////
 
