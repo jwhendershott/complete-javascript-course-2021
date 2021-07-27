eacf714153904968419372788ff12a9557a11382
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-27 00:03:04 -0500

    completed lesson 27

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -285,45 +285,63 @@
 //     console.log(`Someone else should drive.`)
 // }
 
-const day = 'sunday';
+// const day = 'sunday';
 
-switch(day) {
-    case 'monday':
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break
-    case 'friday':
-        console.log('Record videos');
-        break
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend');
-        break
-    default:
-        console.log('Not a valid day');
-}; 
+// switch(day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break
+//     case 'friday':
+//         console.log('Record videos');
+//         break
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break
+//     default:
+//         console.log('Not a valid day');
+// }; 
 
-if (day === 'monday') {
-    console.log('Plan course structure');
-    console.log('Go to coding meetup');
-} else if (day === 'tuesday') {
-    console.log('Prepare theory videos');
-} else if ((day === 'wednesday') || (day === 'thursday')) {
-    console.log('Write code examples');
-} else if (day === 'friday') {
-    console.log('Record videos');
-} else if  ((day === 'saturday') || (day === 'sunday'))  { 
-    console.log('Enjoy the weekend');
-} else {
-    console.log('Not a valid day')
-}
+// if (day === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if ((day === 'wednesday') || (day === 'thursday')) {
+//     console.log('Write code examples');
+// } else if (day === 'friday') {
+//     console.log('Record videos');
+// } else if  ((day === 'saturday') || (day === 'sunday'))  { 
+//     console.log('Enjoy the weekend');
+// } else {
+//     console.log('Not a valid day')
+// }
+
+// Lesson 27: Statements and Expressions
+
+// Expressions:
+3 + 4
+1991
+true && false && !false
+
+// Statements:
+
+if (23 > 10) {
+    const str = '23 is bigger'/*this string is a statement*/;
+};
+
+// Statements cannot be included in template literals.
+console.log(`I'm ${2037 - 1991} years old.`);
+
+
 
 //////////
 
