8b3351face5ecc486c639f61520cf414de1ce345
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-27 23:01:27 -0500

    completed coding challenge 4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -450,6 +450,14 @@
 //     console.log(`Nobody wins.`);
 // };
 
+// Coding challenge #4
+
+const bill = 430
+const tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+console.log(tip);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${tip + bill}.`);
+
 
 //////////
 
