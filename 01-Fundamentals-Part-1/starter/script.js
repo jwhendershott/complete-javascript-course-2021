c324aa1047ad9408c41dc0f94d9560669e930c2d
Jacob Hendershott <jwhendershott@gmail.com>
2021-07-27 22:47:03 -0500

    completed lesson 28

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -327,20 +327,35 @@
 
 // Lesson 27: Statements and Expressions
 
-// Expressions:
-3 + 4
-1991
-true && false && !false
+// // Expressions:
+// 3 + 4
+// 1991
+// true && false && !false
 
-// Statements:
+// // Statements:
 
-if (23 > 10) {
-    const str = '23 is bigger'/*this string is a statement*/;
-};
+// if (23 > 10) {
+//     const str = '23 is bigger'/*this string is a statement*/;
+// };
 
-// Statements cannot be included in template literals.
-console.log(`I'm ${2037 - 1991} years old.`);
+// // Statements cannot be included in template literals.
+// console.log(`I'm ${2037 - 1991} years old.`);
 
+// const age = 23;
+// age >= 21 ? console.log('I like to drink wine.') : console.log('I like to drink water.');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = 'wine';
+// } else {
+//     drink2 = 'water';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}.`);
 
 
 //////////
