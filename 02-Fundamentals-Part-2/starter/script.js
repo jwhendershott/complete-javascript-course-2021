8a9c1976fb5db9bddf87b7c18f137323ddaeb920
Jacob Hendershott <jwhendershott@gmail.com>
2021-08-23 22:19:23 -0500

    began lesson 33

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,27 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log('I can drive');
+// if(passTest) hasDriversLicense = true;
+// if(hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';
 // const private = '12345';
+
+function logger() {
+    console.log('My name is Jonas');
+};
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+};
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
