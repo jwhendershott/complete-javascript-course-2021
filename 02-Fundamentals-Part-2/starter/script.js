43d54837e753b0c86d38f1952dba09744eaefadf
Jacob Hendershott <jwhendershott@gmail.com>
2021-08-29 23:45:17 -0500

    completed lesson 34

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,4 +34,12 @@ function calcAge1(birthYear) {
 };
 
 const age1 = calcAge1(1991);
-console.log(age);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2037- birthYear
+};
+
+const age2 = calcAge2(1991);
+
+console.log(age2);
