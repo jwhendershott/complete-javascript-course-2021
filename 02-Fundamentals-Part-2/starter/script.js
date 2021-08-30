7f7b91b126fb1360c442c453b9ea9ea414405661
Jacob Hendershott <jwhendershott@gmail.com>
2021-08-29 23:38:34 -0500

    began lesson 34

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,23 +9,29 @@
 // const interface = 'Audio';
 // const private = '12345';
 
-function logger() {
-    console.log('My name is Jonas');
+// function logger() {
+//     console.log('My name is Jonas');
+// };
+
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice;
+// };
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number('23');
+// console.log(num);
+
+function calcAge1(birthYear) {
+        return 2037 - birthYear;
 };
 
-logger();
-
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice;
-};
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number('23');
-console.log(num);
-
+const age1 = calcAge1(1991);
+console.log(age);
