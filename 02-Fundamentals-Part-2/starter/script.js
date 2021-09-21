467cf83bcd58dec18c7b2ee612a45e1ae40fa7f1
Jacob Hendershott <jwhendershott@gmail.com>
2021-09-20 22:16:47 -0500

    completed lesson 44

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -213,6 +213,42 @@
 
 // console.log(`${jonas['first' + nameKey]} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`)
 
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function(birthYear){
+    //     return 2037 - birthYear
+    // }
+
+    // calcAge: function(){
+    //     return 2037 - this.birthYear
+    // }
+
+    calcAge: function() {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.` 
+    }
+
+};
+
+// console.log(jonas.calcAge(1991));
+// console.log(jonas.calcAge(jonas.birthYear));
+jonas.calcAge();
+console.log(jonas.age);
+
+// Challenge
+
+console.log(jonas.getSummary())
+
 
 /**********Coding challenges**********/
 
