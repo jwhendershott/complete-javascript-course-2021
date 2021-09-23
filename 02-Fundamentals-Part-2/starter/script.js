72364c6b26113fef6b19dae9d82b2923549a2b51
Jacob Hendershott <jwhendershott@gmail.com>
2021-09-22 21:51:28 -0500

    completed lesson 47

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -254,10 +254,55 @@
 // console.log('Lifting weights repetition 3 🏋️‍♂️');
 // console.log('Lifting weights repetition 4 🏋️‍♂️');
 
-for(let rep = 1; rep  <= 10; rep++) {
-    console.log(`Lifting weights ${rep} 🏋️‍♂️`)
+// for(let rep = 1; rep  <= 10; rep++) {
+//     console.log(`Lifting weights ${rep} 🏋️‍♂️`)
+// }
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037- 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for(let i = 0; i < jonasArray.length; i++) {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+
+    // types[i] = typeof jonasArray[i];
+
+    types.push(typeof jonasArray[i]);
 }
 
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+
+console.log('ONLY STRINGS');
+for(let i = 0; i < jonasArray.length; i++) {
+  if(typeof jonasArray[i] !== 'string') continue;
+  
+    console.log(jonasArray[i], typeof jonasArray[i]);
+} 
+
+console.log('ONLY NUMBER');
+for(let i = 0; i < jonasArray.length; i++) {
+    if(typeof jonasArray[i] === 'number') break;
+    
+      console.log(jonasArray[i], typeof jonasArray[i]);
+  } 
 
 /**********Coding challenges**********/
 
